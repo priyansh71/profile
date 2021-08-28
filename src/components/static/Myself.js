b3702228f9bbbbe51257768ae51abcf73cb77a94
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-08-29 02:31:03 +0530

    added static info about myself

---
+++ src/components/static/Myself.js
@@ -0,0 +1,42 @@
+const Myself = [
+    {
+        "about" : "Hola, I am Priyansh Vyas from Bikaner, Rajasthan, currently a IInd year student pursuing my Engineering degree in Computer Science.",
+        "education" : "I have pursued my education from the following institutions :-",
+        "ongoing" : "I am currently working on adding the following technologies to my techStack :-",
+        "interests" : "Apart from the development and coding part, my interests include :-",
+        "projects" : "I have worked on:- ",
+        "achievments" : "In the academic and learning field, I :-",
+        "certifications" : "I have been certified in :-"
+    },
+    [
+        "BITS Pilani Goa Campus [2020-24], ongoing",
+        "Resonance Eduventures [2018-20]",
+        "Bikaner Boys' School [2008-18]"
+    ],
+    [
+        "Backend Web Development using Node.js.",
+        "App Development using React Native.",
+        "Cryptography using Python and Rust.",
+        "Web Secuity."
+    ],
+    [
+        "Following CtF questions to gain more knowledge in different cybersecurity exploitations.",
+        "Watching Football and Formula 1 the most whenever I am able to.",
+        "Listening to the rock band Linkin Park. I am a huge fan.",
+        "I love terminals. Huge fan of CLI over GUI here."
+
+    ],
+    [
+        "Frontend development projects using static Html and React , utilizing common Css frameworks.",
+        "Basic backend development using Express , Ejs and MongoDb.",
+        "Web scraping and vulnerability testing using popular Python libraries.",
+    ],
+    [
+        "achieved an All India Rank of 3277 amongst 0.2 million eligible candidates in JEE Advanced 2020.",
+        "scored 355/450 in BITSAT-2020.",
+        "achieved a Cumulative Grade Point of 9.68 in my freshman year at BITS Pilani Goa Campus.",
+        "achieved a Top-2 weekly rank in CryptoHack after almost 50+ hours of cryptographic exploration on RSA, Diffie-Hellman Key exchange and Ellipse Curve Cryptography."
+    ]
+]
+
+export default Myself;
